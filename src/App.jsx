49624f2deaa78a95aeede387d38df44ac3d37a54
sunchaserjnr55-a11y <sunchaserjnr55@gmail.com>
2026-04-17 06:01:49 +0500

    Create App component as main application component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to the Ahmii Routine Tracker!</h1>
+            {/* Additional components will go here */}
+        </div>
+    );
+};
+
+export default App;
